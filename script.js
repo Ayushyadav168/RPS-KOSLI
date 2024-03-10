268f296b224c9a936fd5109b72d77b774b535e79
Ayush yadav <156701604+Ayushyadav168@users.noreply.github.com>
2024-03-10 13:55:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+/*const value = { count: 0 };
+gsap.to(value, {
+    count: 3000,
+    roundProps: { count: true },
+    duration: 9,
+    onUpdate: () => {
+        main.innerText = value.count
+    },
+});
+const value = { count: 0 };
+gsap.to(value, {
+    count: 25,
+    roundProps: { count: true },
+    duration: 7,
+    onUpdate: () => {
+        main.innerText = value.count
+    },
+});*/
+/*window.countValue = { count: 0 };
+           gsap.to(window.countValue, {
+             count: 25,
+             roundProps: { count: true },
+             duration: 7,
+             onUpdate: () => {
+               main.innerText = window.countValue.count;
+             },
+           });*/
